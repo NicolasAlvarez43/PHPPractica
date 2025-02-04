8e017c52023b512c3f63e923a0ca6a240b7d4558
NicolasAlvarez43 <105022962+NicolasAlvarez43@users.noreply.github.com>
2025-02-03 23:16:24 -0300

    Proyecto

--- index.php
+++ index.php
@@ -1,4 +1,61 @@
 <?php
 
-echo "hello world"
+#Iniciar curl
+const API_URL = "https://whenisthenextmcufilm.com/api";
+
+$ch = curl_init(API_URL);
+
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
+$data = json_decode($result,true);  // La variable, y lo guarda como array asociativo
+curl_close($ch);
+
+// Hacer lo mismopero otra forma
+$resultv2 = file_get_contents(API_URL);
+//print_r($data)
 ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Centered viewport -->
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+>
+    <title>Document</title>
+</head>
+<main>
+
+<section>
+    <img src="<?=  $data["poster_url"];?>" width="300" > </img>    
+    <hgroup>
+    <h2>  <?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?>   </h2>
+    <p> Fecha de estreno; <?= $data["release_date"]; ?></p>
+    <p>La sig es :   <?= $data["following_production"]["title"]; ?></p>
+</hgroup>
+   
+    </section>
+</main>
+<style>
+    :root{
+        color-scheme: light dark;
+    }
+    body{
+        display: grid;
+        place-content: center;
+    }
+    img{
+        margin: 0 auto
+    }
+    section{
+        display: flex;
+        justify-content: center;
+    }
+    hgroup{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
